a533cc5ba17382dfc226b8f71641a6eb3b6c4d45
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-02-24 09:44:55 -0800

    Fixed a missing view import of UserProfile

--- projects/rango/rango_project/rango/views.py
+++ projects/rango/rango_project/rango/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.bing_search import run_query
 
